937068cc84519e74be666bd26231845239a6832e
phaengkham <lyc.phaengkham@gmail.com>
2024-08-07 11:56:49 +0700

    version 1

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:expenses/wigets/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: Expenses(),
+    ),
+  );
+}
---
+++ lib/wigets/expenses_list/expenses_list.dart
@@ -0,0 +1,20 @@
+import 'package:expenses/models/expense.dart';
+import 'package:expenses/wigets/expenses_list/expens_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
